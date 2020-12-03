69b0550cf4ec762c3e976b5b6f79109045744cab
brianbowen <bbowen1036@gmail.com>
2020-12-02 20:47:59 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js is entry file
+//mongoose entry at top of file
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+//Heroku requires process.env
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
